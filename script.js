2ed74ffa28ce70be82e208cf1fb53cd6dfc5629c
NiZXD471 <55873498+NiZXD471@users.noreply.github.com>
2020-05-17 16:09:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function Onclick1(){
+    window.open("download.html");
+}
+function OnDownload(){
+    setTimeout(download,5000)
+    alert("Download will start soon!, Click ok to start");
+    
+
+}
+
+function download(){
+    window.open("process.html");
+}
